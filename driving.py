ba832f38f52bb5d07e56d915e1644e3498da4f8b
Rick Wang <sho.hsun.wang@gmail.com>
2019-08-09 12:03:33 +0800

    add else part

--- driving.py
+++ driving.py
@@ -3,11 +3,13 @@ age = input('請輸入年齡: ')
 age = int(age)
 if country == '台灣':
     if  age >= 18:
-        print('可以考駕照')
+            print('可以考駕照')
     else:
-        print('不可以駕照')
+            print('不可以駕照')
 elif country == '美國':
     if  age >= 16:
-        print('可以考駕照')
+            print('可以考駕照')
     else:
-        print('不可以駕照')
+            print('不可以駕照')
+else:
+    print('只能輸入台灣和美國')
